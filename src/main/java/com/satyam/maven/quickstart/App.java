71d13b767af068f608b9779ef7d4f459d44705a3
KUMAR SATYAM <satyam.ait@gmail.com>
2018-10-31 12:08:18 +0530

    Update src/main/java/com/satyam/maven/quickstart/App.java
    comment added on 31st

--- src/main/java/com/satyam/maven/quickstart/App.java
+++ src/main/java/com/satyam/maven/quickstart/App.java
@@ -10,6 +10,6 @@ public class App
     public static void main( String[] args )
     {
     	Gson gson = new Gson();
-        System.out.println(gson.toJson("Hello World!") );
+        System.out.println(gson.toJson("Hello World!") );//
     }
 }
